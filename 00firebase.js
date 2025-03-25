823e8476ff56fc48cb51f8a2426b590a79776d62
kumjay061 <144765557+kumjay061@users.noreply.github.com>
2025-03-25 12:24:06 +0530

    Update 00firebase.js

--- 00firebase.js
+++ 00firebase.js
@@ -1,5 +1,4 @@
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDeN0BMkHUoGcjJTDCWqEg1K7ddtObMV94",
   authDomain: "letsupji.firebaseapp.com",
@@ -7,10 +6,12 @@ const firebaseConfig = {
   projectId: "letsupji",
   storageBucket: "letsupji.firebasestorage.app",
   messagingSenderId: "827695233146",
-  appId: "1:827695233146:web:40d770b1219797528b23c3",
+ appId: "1:827695233146:web:40d770b1219797528b23c3",
   measurementId: "G-K3YT31F1ZP"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app)
+
+
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
